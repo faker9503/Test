067e8efb62b823740ba64100a72a9c7986b67e01
rain.yue <rain.yue@goertek.com>
2017-08-19 15:15:46 +0800

    add odd_even.c
    Change-Id: Id6268fac03f2ebcfefeec00a7d331bb9e819953b

--- WIS4/odd_even.c
+++ WIS4/odd_even.c
@@ -1,1 +1,32 @@
-//this file is used for complete the odd or even function
+/******************************************************************************/
+/** To get the input patater is odd or evern
+  *
+  * @param[in] NumInput
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : rain.yue
+  *   Modification : Created function
+
+*******************************************************************************/
+int OddEven( int NumInput )
+{
+    int Num;
+
+    Num = NumInput%2;
+    if( Num == 0 )
+    {
+        printf("输入参数%d为偶数\n",NumInput)；
+    }
+    else
+    {
+        printf("输入参数%d为奇数\n",NumInput);
+    }
+}
+
+//this file is used for complete the remainder function
