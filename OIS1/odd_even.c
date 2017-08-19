b28bac463ecbae7d1c6b15d243ee8e6655ca4294
joshua.ma <joshua.ma@goertek.com>
2017-08-19 15:10:06 +0800

    multiply and oddeven
    Change-Id: I6eb9723070ffb56d8f6c42ac4bd69101e179f331

--- OIS1/odd_even.c
+++ OIS1/odd_even.c
@@ -1,1 +1,31 @@
-//this file is used for complete the odd or even function
+/******************************************************************************/
+/** 奇偶校验函数
+  *
+  * @param[in] data
+  *
+  * @return
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : joshua.ma
+  *   Modification : Created function
+
+*******************************************************************************/
+void OddEvenFunction(int data)
+{
+    if(data % 2 == 0)
+    {
+  	    printf("%d为偶数\n", data);
+  	}
+  	else
+  	{
+  	    printf("%d为奇数\n", data);
+  	}
+}
+
+
+
+/** @}*/
+
+
