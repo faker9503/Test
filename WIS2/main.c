66c46fefb5f1925edf4335b75c88af72c2e62116
doufan.zhang <doufan.zhang@goertek.com>
2017-08-19 17:01:55 +0800

    add main.c
    Change-Id: Id30119b72690d83eaf07cfb17b792ff25eb61ed7

--- WIS2/main.c
+++ WIS2/main.c
@@ -1,24 +1,7 @@
 /*******************************************************************************
             Copyright (C), 2017, GoerTek Technology Co., Ltd.
 *******************************************************************************/
-/** 
-  *  @defgroup	main module name
-  *  @brief 	main 
-  *  @file 		main
-  *  @author 	wilson
-  *  @version 	1.0
-  *  @date    	2017/8/14
-  *  @{
-  *//*
-  *  FileName      : main
-  *  Description   : do the function including add, substract, multiple,
-                     divide,odd or even,
-  *  FunctionList  :
-  * History        :
-  * 1.Date         : 2017/8/14
-  *    Author      : wilson
-  *    Modification: Created file
-*******************************************************************************/
+
 
 /*----------------------------------------------*
  * Include File                                 *
@@ -61,19 +44,21 @@ int main(int argc, char *argv[])
     
   int first_num  = MAX_NUM;
   int second_num = MIN_NUM;
-  OddEven(first_num);
+
     /// the following code to be used for add first_num + second_num functin call
-    
+     add(first_num,second_num);
     /// the following code to be used for subtract first_num - second_num functin call
-     
+     Subtract(first_num,second_num);
     /// the following code to be used for multiply first_num*second_num functin call
-      
+      Multiply(first_num,second_num);
     /// the following code to be used for divide first_num/second_num functin call  
-    
+      divide(first_num,second_num);
     /// the following code to be used for remainder first_num%second_num functin call  
-    
+     int AA;
+     AA=GetRemainder(first_num,second_num);
+     printf("%d\n",AA); 
     /// the following code to be used for odd or even first_num(second_num) is odd or even functin call 
-  Multiply(first_num,second_num);
+      OddEven(first_num);
   system("PAUSE");	
   return 0;
 }
