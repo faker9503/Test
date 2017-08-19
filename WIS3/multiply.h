0e756d410e0618522c6140ef1724c87e68a4c303
hellord.wang <hellord.wang@goertek.com>
2017-08-19 15:52:22 +0800

    WIS3
    Change-Id: I68b3014d5b83d3d68f440be6d720599d615b9843

--- WIS3/multiply.h
+++ WIS3/multiply.h
@@ -1,29 +1,26 @@
 /*******************************************************************************
             Copyright (C), 2017, GoerTek Technology Co., Ltd.
-*******************************************************************************/
-/**
-  *  @defgroup	main module name
-  *  @brief 	main
-  *  @file 		main
-  *  @author 	wilson
+********************************************************************************
+  *  @file 		multiply
+  *  @author 	hellord
   *  @version 	1.0
-  *  @date    	2017/8/14
-  *  @{
-  *//*
-  *  FileName      : main
-  *  Description   : do the function including add, substract, multiple,
-                     divide,odd or even,
+  *  @date    	2017/8/19
+  *  FileName      : multiply
+  *  Description   : multiply.c's Head File
   *  FunctionList  :
   * History        :
-  * 1.Date         : 2017/8/14
-  *    Author      : wilson
+  * 1.Date         : 2017/8/19
+  *    Author      : hellord
   *    Modification: Created file
 *******************************************************************************/
+
+#ifndef __MULTIPLY_H__
+#define __MULTIPLY_H__
 
 /*----------------------------------------------*
  * Include File                                 *
  *----------------------------------------------*/
-#include "private.h"
+
 /*----------------------------------------------*
  * External Declaration                         *
  *----------------------------------------------*/
@@ -51,42 +48,12 @@
 /*----------------------------------------------*
  * Micro Defination                             *
  *----------------------------------------------*/
-#define MAX_NUM 12
-#define MIN_NUM 9
+
 /*----------------------------------------------*
  * Function Declaration                         *
  *----------------------------------------------*/
-int main()
-{
-
-  int first_num  = MAX_NUM;
-  int second_num = MIN_NUM;
-    /// the following code to be used for add first_num + second_num functin call
-
-    /// the following code to be used for subtract first_num - second_num functin call
-
-    /// the following code to be used for multiply first_num*second_num functin call
-
-    /// the following code to be used for divide first_num/second_num functin call
-
-    /// the following code to be used for remainder first_num%second_num functin call
-
-    /// the following code to be used for odd or even first_num(second_num) is odd or even functin call
-  int mul_num = Multip(first_num, second_num);
-  int add_num = Add(first_num, second_num);
-  int div_num = Divide(first_num, second_num);
-  int rem_num = remainder(first_num, second_num);
-  int sub_num = sub(first_num, second_num);
-  int if_odd = OddOrEven(first_num);
-  printf("12*9=%d\n", mul_num);
-  printf("12+9=%d\n", add_num);
-  printf("12/9=%d\n", div_num);
-  printf("12%9=%d\n", rem_num);
-  printf("12-9=%d\n", sub_num);
-  printf("12ÊÇ·ñÊÇÆæÊý%d\n", if_odd);
-  system("PAUSE");
-  return 0;
-}
 
 
-/** @}*/
+int  Multip( int num1, int num2 );
+
+#endif /* __MULTIPLY_H__ */
