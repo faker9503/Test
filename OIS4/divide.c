66537e33d282a9ffd71adf3ead77f0d949fb4fec
carpenter.zhang <carpenter.zhang@goertek.com>
2017-08-19 14:21:17 +0800

    add divide.c remainder.c
    Change-Id: If8ca51a65d07b1d8b26b918335bd253b69b9c243

--- OIS4/divide.c
+++ OIS4/divide.c
@@ -1,1 +1,26 @@
 //this file is used for complete the divide function
+/******************************************************************************/
+/** divide
+  *
+  * @param[in] first_num
+  * @param[in] second_num
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : carpenter.zhang
+  *   Modification : Created function
+
+*******************************************************************************/
+int DivideFunc( int first_num, int second_num )
+{
+    int divide;
+    
+    divide = first_num / second_num;
+    
+    return divide;
+}
