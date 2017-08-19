b8181b042981a4d2079be6b3be7747968d92b043
lights.chang <lights.chang@goertek.com>
2017-08-19 14:28:43 +0800

    添加求和函数c文件和求和头文件
    Change-Id: I92e7fb88bd070db173361623630c262da9cf1600

--- WIS3/add.c
+++ WIS3/add.c
@@ -1,1 +1,29 @@
 //this file is used for complete the add function
+/******************************************************************************/
+/** 求和
+  *
+  * @param[in] add_num_one
+  * @param[in] add_num_teo
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : lights.chang
+  *   Modification : Created function
+
+*******************************************************************************/
+int Add( int add_num_one, int add_num_two )
+{
+    /*保存两个数的和*/
+    int add_sum;
+
+    /*求取两个数的和*/
+    add_sum = add_num_one + add_num_two;
+
+    /*返回两个数的和*/
+    return add_sum;
+}
