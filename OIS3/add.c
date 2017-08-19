034320ede938fe0be12269fb7dd43872afeb0d4d
faker.yang <faker.yang@goertek.com>
2017-08-19 14:43:08 +0800

    faker‘s code
    Change-Id: I631d4d4da32fba6aff28ff02ac376db072f4e155

--- OIS3/add.c
+++ OIS3/add.c
@@ -1,1 +1,21 @@
-//this file is used for complete the add function
+/******************************************************************************/
+/**  This function is to implement addition operations
+  *
+  * @param[in] first_num
+  * @param[in] second_num
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : nelly
+  *   Modification : Created function
+
+*******************************************************************************/
+int Add(int first_num, int second_num)
+{
+    return first_num + second_num;
+}
--- OIS3/main.c
+++ OIS3/main.c
@@ -1,9 +1,9 @@
 /*******************************************************************************
             Copyright (C), 2017, GoerTek Technology Co., Ltd.
 *******************************************************************************/
-/** 
+/**
   *  @defgroup	main module name
-  *  @brief 	main 
+  *  @brief 	main
   *  @file 		main
   *  @author 	wilson
   *  @version 	1.0
@@ -58,22 +58,29 @@
  *----------------------------------------------*/
 int main(int argc, char *argv[])
 {
-    
+
   int first_num  = MAX_NUM;
   int second_num = MIN_NUM;
-    /// the following code to be used for add first_num + second_num functin call
-    
-    /// the following code to be used for subtract first_num - second_num functin call
-     
-    /// the following code to be used for multiply first_num*second_num functin call
-      
-    /// the following code to be used for divide first_num/second_num functin call  
-    
-    /// the following code to be used for remainder first_num%second_num functin call  
-    
-    /// the following code to be used for odd or even first_num(second_num) is odd or even functin call 
-  
-  system("PAUSE");	
+  /// the following code to be used for add first_num + second_num functin call
+  printf("first_num + second_num = %d\n",Add(first_num,second_num));
+  /// the following code to be used for subtract first_num - second_num functin call
+  printf("first_num - second_num = %d\n",Subtract(first_num,second_num));
+  /// the following code to be used for multiply first_num*second_num functin call
+  printf("first_num * second_num = %d\n",Multiply(first_num,second_num));
+  /// the following code to be used for divide first_num/second_num functin call
+  printf("first_num / second_num = %d\n",Divide(first_num,second_num));
+  /// the following code to be used for remainder first_num%second_num functin call
+  printf("first_num mod second_num = %d\n",GetRemainder(first_num,second_num));
+  /// the following code to be used for odd or even first_num(second_num) is odd or even functin call
+  if(IsEven(first_num))
+  {
+    puts("the num is even");
+  }
+  else
+  {
+    puts("the num is odd");
+  }
+  system("PAUSE");
   return 0;
 }
 
