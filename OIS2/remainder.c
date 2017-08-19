bd98356347e6e66e59bcfb3441ef539184daca58
foamy.zang <foamy.zang@goertek.com>
2017-08-19 15:46:34 +0800

    add remainder.c
    Change-Id: Ibd7b8dfb222e44932e0a8ec26a5d289faa40fba7

--- OIS2/remainder.c
+++ OIS2/remainder.c
@@ -1,1 +1,47 @@
-//this file is used for complete the remainder function
+/*******************************************************************************
+            Copyright (C), 2017, GoerTek Technology Co., Ltd.
+*******************************************************************************/
+/**
+  *  @defgroup	remainder module name
+  *  @brief 	remainder
+  *  @file 		remainder
+  *  @author 	foamy.zang
+  *  @version 	1.0
+  *  @date    	2017/8/19
+  *  @{
+  *//*
+  *  FileName      : remainder
+  *  Description   : remainder
+  *  FunctionList  :
+  * History        :
+  * 1.Date         : 2017/8/19
+  *    Author      : foamy.zang
+  *    Modification: Created file
+*******************************************************************************/
+
+#include "Private.h"
+
+/*****************************************************************************/
+/** num1 and num2 remainder
+  *
+  * @param[in] num2
+  * @param[in] num1
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : foamy.zang
+  *   Modification : Created function
+
+******************************************************************************/
+int Remainder( int num1 ,int num2 )
+{
+    int rem; //rem is the remainder
+    rem = num1 % num2;//calculate the remainder of two numbers
+    return rem;
+}
+
