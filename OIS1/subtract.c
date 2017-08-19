5fd089f5aad576e7745e9b6ad771a49855d78d66
emily.dai <emily.dai@goertek.com>
2017-08-19 14:53:09 +0800

    add subtract.c
    Change-Id: I16c0d0045e0f3cecc0c3a3a44866fcc3d0548808

--- OIS1/subtract.c
+++ OIS1/subtract.c
@@ -1,1 +1,25 @@
 //this file is used for complete the subtract function
+/******************************************************************************/
+/** 实现减功能
+  *
+  * @param[in] first_num
+  * @param[in] second_num
+  *
+  * @return
+  *
+  * @note
+  *
+  *//*
+  * History        :
+  * 1.Date         : 2017/8/19
+  *   Author       : emily
+  *   Modification : Created function
+
+*******************************************************************************/
+#include"Private.h"
+
+int Subtract(int first_num, int second_num)
+{
+    return first_num - second_num;
+}
+
